7475ddfc0a9f73cf2c6b368756d1b5b78af2aad7
vicky <vickykumarsunilja@gmail.com>
2024-04-09 17:15:52 +0530

    Setup the mongodb database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is running on ", PORT);
+});
+mongoose.connect(process.env.MONGO_DATABASE_URL!).then(() => {
+  console.log("Mongo database connected");
 });
